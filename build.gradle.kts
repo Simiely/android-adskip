ccbf4f1e42bff7f3f79c44f5ec41020e8496cb7d
Simiely <124016031+Simiely@users.noreply.github.com>
2026-07-26 00:08:25 +0800

    fix: add aliyun maven mirrors

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,3 +3,13 @@ plugins {
     id("com.android.application") version "8.5.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.24" apply false
 }
+
+allprojects {
+    repositories {
+        maven { url = uri("https://maven.aliyun.com/repository/public") }
+        maven { url = uri("https://maven.aliyun.com/repository/google") }
+        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
+        google()
+        mavenCentral()
+    }
+}
